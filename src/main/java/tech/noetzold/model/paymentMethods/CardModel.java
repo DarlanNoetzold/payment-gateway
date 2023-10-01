8f6fd65758c226d6d043f7fb67bbf548edc52579
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-01 19:19:22 -0300

    Create CardModel.java

---
+++ src/main/java/tech/noetzold/model/paymentMethods/CardModel.java
@@ -0,0 +1,28 @@
+package tech.noetzold.model.paymentMethods;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import tech.noetzold.model.enums.CardType;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class CardModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String cardNumber;
+    private String cardHolderName;
+    private String expirationDate;
+    private String cvv;
+    private CardType cardType;
+}

09641e3d9c7f2911c63ea0542b3acfd0a38e688b
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-01 19:19:28 -0300

    Update PaymentModel.java

--- src/main/java/tech/noetzold/model/PaymentModel.java
+++ src/main/java/tech/noetzold/model/PaymentModel.java
@@ -1,4 +1,42 @@
 package tech.noetzold.model;
 
+import jakarta.persistence.*;
+import org.wildfly.common.annotation.NotNull;
+import tech.noetzold.model.enums.PaymentMethod;
+import tech.noetzold.model.paymentMethods.BoletoModel;
+import tech.noetzold.model.paymentMethods.CardModel;
+import tech.noetzold.model.paymentMethods.PaypalModel;
+import tech.noetzold.model.paymentMethods.PixModel;
+
+import java.util.UUID;
+
 public class PaymentModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private CustomerModel customer;
+    @NotNull
+    private PaymentMethod paymentMethod;
+    @NotNull
+    private double totalAmount;
+    private double dicountAmount;
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private BoletoModel boletoModel;
+    @ManyToOne
+    @JoinColumn(name = "card_model_id")
+    private CardModel cardModel;
+    @ManyToOne
+    @JoinColumn(name = "pix_model_id")
+    private PixModel pixModel;
+    @ManyToOne
+    @JoinColumn(name = "paypal_model_id")
+    private PaypalModel paypalModel;
+
+
 }

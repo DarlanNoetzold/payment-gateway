83ad67791607110677a5171ae626ce39ade132da
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-01 19:19:26 -0300

    Create PixModel.java

---
+++ src/main/java/tech/noetzold/model/paymentMethods/PixModel.java
@@ -0,0 +1,25 @@
+package tech.noetzold.model.paymentMethods;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class PixModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String chavePix;
+    private String descricao;
+    private String identificadorTransacao;
+}

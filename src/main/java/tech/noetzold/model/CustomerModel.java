1ba3031f2c960870cf9b434bee61680c466afff3
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-01 18:57:33 -0300

    Update CustomerModel.java

--- src/main/java/tech/noetzold/model/CustomerModel.java
+++ src/main/java/tech/noetzold/model/CustomerModel.java
@@ -1,4 +1,42 @@
 package tech.noetzold.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.wildfly.common.annotation.NotNull;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class CustomerModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotNull
+    private String userId;
+
+    private Date registerDate;
+
+    private Date bornDate;
+
+    @OneToMany
+    @JoinColumn(name = "customer_id")
+    private List<MoneyTransaction> transactions;
+
+    @OneToMany
+    @JoinColumn(name = "customer_id")
+    private List<MoneyTransaction> transactionsWithError;
+
+    @OneToMany
+    @JoinColumn(name = "customer_id")
+    private List<CardModel> cards;
+
 }

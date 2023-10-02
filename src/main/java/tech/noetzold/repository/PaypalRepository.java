269dce840afe6511af778be866de744217b2ea2c
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-02 08:22:02 -0300

    Update PaypalRepository.java

--- src/main/java/tech/noetzold/repository/PaypalRepository.java
+++ src/main/java/tech/noetzold/repository/PaypalRepository.java
@@ -4,6 +4,16 @@ import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 import tech.noetzold.model.paymentMethods.PaypalModel;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @ApplicationScoped
 public class PaypalRepository implements PanacheRepository<PaypalModel> {
+    public Optional<PaypalModel> findByIdOptional(UUID id) {
+        return find("id", id).firstResultOptional();
+    }
+
+    public void deleteById(UUID id) {
+        delete("id", id);
+    }
 }

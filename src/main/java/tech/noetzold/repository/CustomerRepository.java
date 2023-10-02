4749774ef9473807944815123b0850ec34046ff2
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-02 08:13:42 -0300

    Update CustomerRepository.java

--- src/main/java/tech/noetzold/repository/CustomerRepository.java
+++ src/main/java/tech/noetzold/repository/CustomerRepository.java
@@ -4,6 +4,16 @@ import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 import tech.noetzold.model.CustomerModel;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @ApplicationScoped
 public class CustomerRepository implements PanacheRepository<CustomerModel> {
+    public Optional<CustomerModel> findByIdOptional(UUID id) {
+        return find("id", id).firstResultOptional();
+    }
+
+    public void deleteById(UUID id) {
+        delete("id", id);
+    }
 }

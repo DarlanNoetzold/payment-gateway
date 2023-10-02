dd4963e4e63dc4c6abe06b2621beba7096f843b7
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-02 08:13:46 -0300

    Update BoletoService.java

--- src/main/java/tech/noetzold/service/BoletoService.java
+++ src/main/java/tech/noetzold/service/BoletoService.java
@@ -17,51 +17,51 @@ public class BoletoService {
     BoletoRepository boletoRepository;
 
 
-    private BoletoModel findById(UUID id){
-        Optional<BoletoModel> optionalAlert = boletoRepository.findByIdOptional(id);
-        return optionalAlert.orElse(null);
+    public BoletoModel findBoletoModelById(UUID id){
+        Optional<BoletoModel> optionalBoletoModel = boletoRepository.findByIdOptional(id);
+        return optionalBoletoModel.orElse(null);
     }
 
-    private void saveBoletoModel(BoletoModel boletoModel){
+    public void saveBoletoModel(BoletoModel boletoModel){
         boletoRepository.persist(boletoModel);
     }
 
-    private void updateBoletoModel(BoletoModel boletoModel){
+    public void updateBoletoModel(BoletoModel boletoModel){
         if (boletoModel == null || boletoModel.getId() == null) {
             throw new WebApplicationException("Invalid data for boletoModel update", Response.Status.BAD_REQUEST);
         }
 
-        BoletoModel existingboletoModel = findById(boletoModel.getId());
-        if (existingboletoModel == null) {
+        BoletoModel existingBoletoModel = findBoletoModelById(boletoModel.getId());
+        if (existingBoletoModel == null) {
             throw new WebApplicationException("boletoModel not found", Response.Status.NOT_FOUND);
         }
 
-        existingboletoModel.setCodigoDeBarras(boletoModel.getCodigoDeBarras());
-        existingboletoModel.setLinhaDigitavel(boletoModel.getLinhaDigitavel());
-        existingboletoModel.setBeneficiarioNome(boletoModel.getBeneficiarioNome());
-        existingboletoModel.setBeneficiarioCnpjCpf(boletoModel.getBeneficiarioCnpjCpf());
-        existingboletoModel.setBeneficiarioEndereco(boletoModel.getBeneficiarioEndereco());
-        existingboletoModel.setSacadoNome(boletoModel.getSacadoNome());
-        existingboletoModel.setSacadoCnpjCpf(boletoModel.getSacadoCnpjCpf());
-        existingboletoModel.setSacadoEndereco(boletoModel.getSacadoEndereco());
-        existingboletoModel.setValor(boletoModel.getValor());
-        existingboletoModel.setDataVencimento(boletoModel.getDataVencimento());
-        existingboletoModel.setNumeroDocumento(boletoModel.getNumeroDocumento());
-        existingboletoModel.setInstrucoesPagamento(boletoModel.getInstrucoesPagamento());
-        existingboletoModel.setMulta(boletoModel.getMulta());
-        existingboletoModel.setJurosAtraso(boletoModel.getJurosAtraso());
-        existingboletoModel.setLogoBanco(boletoModel.getLogoBanco());
-        existingboletoModel.setCodigoBanco(boletoModel.getCodigoBanco());
-        existingboletoModel.setIdentificacaoBoleto(boletoModel.getIdentificacaoBoleto());
-        existingboletoModel.setDataEmissao(boletoModel.getDataEmissao());
-        existingboletoModel.setLocalPagamento(boletoModel.getLocalPagamento());
-        existingboletoModel.setCodigoAutenticacao(boletoModel.getCodigoAutenticacao());
-        existingboletoModel.setInformacoesContato(boletoModel.getInformacoesContato());
+        existingBoletoModel.setCodigoDeBarras(boletoModel.getCodigoDeBarras());
+        existingBoletoModel.setLinhaDigitavel(boletoModel.getLinhaDigitavel());
+        existingBoletoModel.setBeneficiarioNome(boletoModel.getBeneficiarioNome());
+        existingBoletoModel.setBeneficiarioCnpjCpf(boletoModel.getBeneficiarioCnpjCpf());
+        existingBoletoModel.setBeneficiarioEndereco(boletoModel.getBeneficiarioEndereco());
+        existingBoletoModel.setSacadoNome(boletoModel.getSacadoNome());
+        existingBoletoModel.setSacadoCnpjCpf(boletoModel.getSacadoCnpjCpf());
+        existingBoletoModel.setSacadoEndereco(boletoModel.getSacadoEndereco());
+        existingBoletoModel.setValor(boletoModel.getValor());
+        existingBoletoModel.setDataVencimento(boletoModel.getDataVencimento());
+        existingBoletoModel.setNumeroDocumento(boletoModel.getNumeroDocumento());
+        existingBoletoModel.setInstrucoesPagamento(boletoModel.getInstrucoesPagamento());
+        existingBoletoModel.setMulta(boletoModel.getMulta());
+        existingBoletoModel.setJurosAtraso(boletoModel.getJurosAtraso());
+        existingBoletoModel.setLogoBanco(boletoModel.getLogoBanco());
+        existingBoletoModel.setCodigoBanco(boletoModel.getCodigoBanco());
+        existingBoletoModel.setIdentificacaoBoleto(boletoModel.getIdentificacaoBoleto());
+        existingBoletoModel.setDataEmissao(boletoModel.getDataEmissao());
+        existingBoletoModel.setLocalPagamento(boletoModel.getLocalPagamento());
+        existingBoletoModel.setCodigoAutenticacao(boletoModel.getCodigoAutenticacao());
+        existingBoletoModel.setInformacoesContato(boletoModel.getInformacoesContato());
 
-        boletoRepository.persist(existingboletoModel);
+        boletoRepository.persist(existingBoletoModel);
     }
 
-    private void deleteBoletoModel(UUID id){
+    public void deleteBoletoModelById(UUID id){
         boletoRepository.deleteById(id);
     }
 }

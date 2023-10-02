42945cdf66f362cc2db9f685b8d05f188e967d3e
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-01 23:12:11 -0300

    Update BoletoRepository.java

--- src/main/java/tech/noetzold/repository/BoletoRepository.java
+++ src/main/java/tech/noetzold/repository/BoletoRepository.java
@@ -4,6 +4,16 @@ import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 import tech.noetzold.model.paymentMethods.BoletoModel;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @ApplicationScoped
 public class BoletoRepository implements PanacheRepository<BoletoModel> {
+    public Optional<BoletoModel> findByIdOptional(UUID id) {
+        return find("id", id).firstResultOptional();
+    }
+
+    public void deleteById(UUID id) {
+        delete("id", id);
+    }
 }

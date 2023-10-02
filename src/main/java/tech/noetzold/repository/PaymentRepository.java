439e499d553bd6c95ca31e99560aef63c9fe5040
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-02 08:13:44 -0300

    Update PaymentRepository.java

--- src/main/java/tech/noetzold/repository/PaymentRepository.java
+++ src/main/java/tech/noetzold/repository/PaymentRepository.java
@@ -4,6 +4,16 @@ import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 import tech.noetzold.model.PaymentModel;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @ApplicationScoped
 public class PaymentRepository implements PanacheRepository<PaymentModel> {
+    public Optional<PaymentModel> findByIdOptional(UUID id) {
+        return find("id", id).firstResultOptional();
+    }
+
+    public void deleteById(UUID id) {
+        delete("id", id);
+    }
 }

5358d1ef4ef2aa88710a29313415270e1797b844
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-02 08:22:08 -0300

    Update PixService.java

--- src/main/java/tech/noetzold/service/PixService.java
+++ src/main/java/tech/noetzold/service/PixService.java
@@ -3,11 +3,47 @@ package tech.noetzold.service;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Response;
+import tech.noetzold.model.paymentMethods.PixModel;
 import tech.noetzold.repository.PixRepository;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @ApplicationScoped
 public class PixService {
 
     @Inject
     PixRepository pixRepository;
+
+    public PixModel findPixModelById(UUID id){
+        Optional<PixModel> optionalPixModel = pixRepository.findByIdOptional(id);
+        return optionalPixModel.orElse(null);
+    }
+
+    public void savePixModel(PixModel pixModel){
+        pixRepository.persist(pixModel);
+    }
+
+    public void updatePixModel(PixModel pixModel){
+        if (pixModel == null || pixModel.getId() == null) {
+            throw new WebApplicationException("Invalid data for pixModel update", Response.Status.BAD_REQUEST);
+        }
+
+        PixModel existingPixModel = findPixModelById(pixModel.getId());
+        if (existingPixModel == null) {
+            throw new WebApplicationException("customerModel not found", Response.Status.NOT_FOUND);
+        }
+
+        existingPixModel.setChavePix(pixModel.getChavePix());
+        existingPixModel.setDescricao(pixModel.getDescricao());
+        existingPixModel.setIdentificadorTransacao(pixModel.getIdentificadorTransacao());
+
+        pixRepository.persist(existingPixModel);
+    }
+
+    public void deletePixModelById(UUID id){
+        pixRepository.deleteById(id);
+    }
 }

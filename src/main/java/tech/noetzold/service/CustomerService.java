609522fd348073c122c48159289a5ae4e4b18d0c
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-01 22:38:27 -0300

    Update CustomerService.java

--- src/main/java/tech/noetzold/service/CustomerService.java
+++ src/main/java/tech/noetzold/service/CustomerService.java
@@ -1,4 +1,12 @@
 package tech.noetzold.service;
 
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import tech.noetzold.repository.CustomerRepository;
+
+@ApplicationScoped
 public class CustomerService {
+
+    @Inject
+    CustomerRepository customerRepository;
 }

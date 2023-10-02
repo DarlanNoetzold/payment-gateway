51fc8497f323c754e323eb1aff1722450682cb31
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-02 09:31:07 -0300

    Update CustomerController.java

--- src/main/java/tech/noetzold/controller/CustomerController.java
+++ src/main/java/tech/noetzold/controller/CustomerController.java
@@ -1,4 +1,13 @@
 package tech.noetzold.controller;
 
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/api/v1/customer")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class CustomerController {
 }

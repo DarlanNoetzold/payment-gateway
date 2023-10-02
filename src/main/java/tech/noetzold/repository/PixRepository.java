96728be5453810b6c2d8fb2bb1fe9a0309feb6e3
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-02 08:22:05 -0300

    Update PixRepository.java

--- src/main/java/tech/noetzold/repository/PixRepository.java
+++ src/main/java/tech/noetzold/repository/PixRepository.java
@@ -4,6 +4,17 @@ import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 import tech.noetzold.model.paymentMethods.PixModel;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @ApplicationScoped
 public class PixRepository implements PanacheRepository<PixModel> {
+
+    public Optional<PixModel> findByIdOptional(UUID id) {
+        return find("id", id).firstResultOptional();
+    }
+
+    public void deleteById(UUID id) {
+        delete("id", id);
+    }
 }

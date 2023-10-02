153d40e70fa739a5137fe564f014eca89cef4886
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-02 08:13:40 -0300

    Update CardRepository.java

--- src/main/java/tech/noetzold/repository/CardRepository.java
+++ src/main/java/tech/noetzold/repository/CardRepository.java
@@ -2,8 +2,19 @@ package tech.noetzold.repository;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
+import tech.noetzold.model.paymentMethods.BoletoModel;
 import tech.noetzold.model.paymentMethods.CardModel;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @ApplicationScoped
 public class CardRepository implements PanacheRepository<CardModel> {
+    public Optional<CardModel> findByIdOptional(UUID id) {
+        return find("id", id).firstResultOptional();
+    }
+
+    public void deleteById(UUID id) {
+        delete("id", id);
+    }
 }

bfeae100d92b9d0c4be49e1a2ebc630a79a912cf
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-03 06:26:49 -0300

    Update CustomerController.java

--- src/main/java/tech/noetzold/controller/CustomerController.java
+++ src/main/java/tech/noetzold/controller/CustomerController.java
@@ -1,16 +1,28 @@
 package tech.noetzold.controller;
 
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.jboss.logging.Logger;
+import tech.noetzold.model.CustomerModel;
+import tech.noetzold.service.CustomerService;
 
 @Path("/api/v1/customer")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class CustomerController {
 
+    @Inject
+    CustomerService customerService;
 
+    @Channel("customers")
+    Emitter<CustomerModel> quoteRequestEmitter;
+
+    private static final Logger logger = Logger.getLogger(CustomerController.class);
 
 }

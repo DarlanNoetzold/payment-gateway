33d96a48a1d8fd927c96cbb3239df95b7a8bd6c8
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-03 06:55:06 -0300

    Update CustomerController.java

--- src/main/java/tech/noetzold/controller/CustomerController.java
+++ src/main/java/tech/noetzold/controller/CustomerController.java
@@ -1,16 +1,18 @@
 package tech.noetzold.controller;
 
 
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
 import org.jboss.logging.Logger;
 import tech.noetzold.model.CustomerModel;
 import tech.noetzold.service.CustomerService;
+
+import java.util.UUID;
 
 @Path("/api/v1/customer")
 @Produces(MediaType.APPLICATION_JSON)
@@ -24,5 +26,35 @@ public class CustomerController {
     Emitter<CustomerModel> quoteRequestEmitter;
 
     private static final Logger logger = Logger.getLogger(CustomerController.class);
+
+    @GET
+    @RolesAllowed("admin")
+    public Response getCustomerModelByUserId(@PathParam("id") String id){
+
+        CustomerModel customerModel = customerService.findCustomerModelByUserId(id);
+
+        if(customerModel == null){
+            logger.error("There is no customer with userId: " + id);
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        return Response.ok(customerModel).build();
+    }
+
+    @GET
+    @Path("/paymentId")
+    @RolesAllowed("admin")
+    public Response getCustomerModelById(@PathParam("id") String id){
+        UUID uuid = UUID.fromString(id);
+
+        CustomerModel customerModel = customerService.findCustomerModelById(uuid);
+
+        if(customerModel == null){
+            logger.error("There is no customer with id: " + id);
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        return Response.ok(customerModel).build();
+    }
 
 }

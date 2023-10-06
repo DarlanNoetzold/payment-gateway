f53a3f1c48534c542dd2b607600c0ac579b8595f
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-06 06:24:10 -0300

    Update PaymentController.java

--- src/main/java/tech/noetzold/controller/PaymentController.java
+++ src/main/java/tech/noetzold/controller/PaymentController.java
@@ -1,15 +1,47 @@
 package tech.noetzold.controller;
 
 
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.jboss.logging.Logger;
+import tech.noetzold.model.CustomerModel;
+import tech.noetzold.model.PaymentModel;
+import tech.noetzold.service.PaymentService;
+
+import java.util.UUID;
 
 @Path("/api/v1/payment")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class PaymentController {
 
+    @Inject
+    PaymentService paymentService;
+
+    @Channel("customers")
+    Emitter<CustomerModel> quoteRequestEmitter;
+
+    private static final Logger logger = Logger.getLogger(CustomerController.class);
+
+    @GET
+    @Path("/{id}")
+    @RolesAllowed("admin")
+    public Response findPaymentModelById(@PathParam("id") String id) {
+        UUID uuid = UUID.fromString(id);
+
+        PaymentModel paymentModel = paymentService.findPaymentModelById(uuid);
+
+        if (paymentModel == null) {
+            logger.error("There is no payment with id: " + id);
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        return Response.ok(paymentModel).build();
+    }
 
 }

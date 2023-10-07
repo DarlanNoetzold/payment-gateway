cddf7b29a441c177be5ff65279538cecbb75e3a7
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-07 00:34:45 -0300

    Update PaymentConsumer.java

--- src/main/java/tech/noetzold/consumer/PaymentConsumer.java
+++ src/main/java/tech/noetzold/consumer/PaymentConsumer.java
@@ -1,17 +1,17 @@
 package tech.noetzold.consumer;
 
 import io.smallrye.reactive.messaging.annotations.Blocking;
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import tech.noetzold.model.CustomerModel;
 import tech.noetzold.model.PaymentModel;
-import tech.noetzold.service.CustomerService;
 import tech.noetzold.service.PaymentService;
 
 import java.util.Date;
 
+@ApplicationScoped
 public class PaymentConsumer {
 
     @Inject
@@ -25,7 +25,7 @@ public class PaymentConsumer {
     public PaymentModel process(PaymentModel incomingPaymentMode) {
         incomingPaymentMode.setRegisterDate(new Date());
         paymentService.savePaymentModel(incomingPaymentMode);
-        logger.info("Create Customer " + incomingPaymentMode.getId() + " for user " + incomingPaymentMode.getCustomer().getUserId() + ".");
+        logger.info("Create payments " + incomingPaymentMode.getId() + " for user " + incomingPaymentMode.getCustomer().getUserId() + ".");
 
         return incomingPaymentMode;
     }

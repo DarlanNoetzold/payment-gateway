1f11b16908ac39526290350115cfc3ecc63c7e1e
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-07 11:33:13 -0300

    Update PaymentConsumer.java

--- src/main/java/tech/noetzold/consumer/PaymentConsumer.java
+++ src/main/java/tech/noetzold/consumer/PaymentConsumer.java
@@ -7,8 +7,14 @@ import jakarta.inject.Inject;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import tech.noetzold.model.CustomerModel;
 import tech.noetzold.model.PaymentModel;
-import tech.noetzold.service.PaymentService;
+import tech.noetzold.model.enums.PaymentMethod;
+import tech.noetzold.model.paymentMethods.BoletoModel;
+import tech.noetzold.model.paymentMethods.CardModel;
+import tech.noetzold.model.paymentMethods.PaypalModel;
+import tech.noetzold.model.paymentMethods.PixModel;
+import tech.noetzold.service.*;
 
 import java.util.Date;
 
@@ -18,6 +24,20 @@ public class PaymentConsumer {
     @Inject
     PaymentService paymentService;
 
+    @Inject
+    CustomerService customerService;
+
+    @Inject
+    PaypalService paypalService;
+
+    @Inject
+    CardService cardService;
+
+    @Inject
+    PixService pixService;
+
+    @Inject
+    BoletoService boletoService;
 
     private static final Logger logger = LoggerFactory.getLogger(PaymentConsumer.class);
 
@@ -25,6 +45,31 @@ public class PaymentConsumer {
     @Merge
     @Blocking
     public PaymentModel process(PaymentModel incomingPaymentMode) {
+
+        CustomerModel customerModel = customerService.findCustomerModelByUserId(incomingPaymentMode.getCustomer().getUserId());
+
+        if (customerModel == null){
+            customerModel = customerService.saveCustomerModel(incomingPaymentMode.getCustomer());
+        }
+
+        if (incomingPaymentMode.getPaymentMethod().equals(PaymentMethod.PAYPAL)){
+            PaypalModel paypalModel = paypalService.savePaypalModel(incomingPaymentMode.getPaypalModel());
+            incomingPaymentMode.setPaypalModel(paypalModel);
+        } else if (incomingPaymentMode.getPaymentMethod().equals(PaymentMethod.DEBIT_CARD) || incomingPaymentMode.getPaymentMethod().equals(PaymentMethod.CREDIT_CARD)) {
+            CardModel cardModel = cardService.saveCardModel(incomingPaymentMode.getCardModel());
+            incomingPaymentMode.setCardModel(cardModel);
+        } else if (incomingPaymentMode.getPaymentMethod().equals(PaymentMethod.PIX)) {
+            PixModel pixModel = pixService.savePixModel(incomingPaymentMode.getPixModel());
+            incomingPaymentMode.setPixModel(pixModel);
+        } else if (incomingPaymentMode.getPaymentMethod().equals(PaymentMethod.BOLETO)) {
+            BoletoModel boletoModel = boletoService.saveBoletoModel(incomingPaymentMode.getBoletoModel());
+            incomingPaymentMode.setBoletoModel(boletoModel);
+        } else {
+            logger.error("Lost message info.");
+            return null;
+        }
+
+        incomingPaymentMode.setCustomer(customerModel);
         incomingPaymentMode.setRegisterDate(new Date());
         paymentService.savePaymentModel(incomingPaymentMode);
         logger.info("Create payments " + incomingPaymentMode.getId() + " for user " + incomingPaymentMode.getCustomer().getUserId() + ".");

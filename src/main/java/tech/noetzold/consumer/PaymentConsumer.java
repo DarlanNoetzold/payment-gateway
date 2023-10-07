05be66f381b62e87e1d001a4a24db06c7347a173
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-07 00:40:21 -0300

    Update PaymentConsumer.java

--- src/main/java/tech/noetzold/consumer/PaymentConsumer.java
+++ src/main/java/tech/noetzold/consumer/PaymentConsumer.java
@@ -1,6 +1,7 @@
 package tech.noetzold.consumer;
 
 import io.smallrye.reactive.messaging.annotations.Blocking;
+import io.smallrye.reactive.messaging.annotations.Merge;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
@@ -21,6 +22,7 @@ public class PaymentConsumer {
     private static final Logger logger = LoggerFactory.getLogger(PaymentConsumer.class);
 
     @Incoming("payments")
+    @Merge
     @Blocking
     public PaymentModel process(PaymentModel incomingPaymentMode) {
         incomingPaymentMode.setRegisterDate(new Date());

3c33238e17d50212159338f9c7b16602625261ec
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-07 23:18:21 -0300

    Create InvoiceConsumer.java

---
+++ src/main/java/tech/noetzold/consumer/InvoiceConsumer.java
@@ -0,0 +1,34 @@
+package tech.noetzold.consumer;
+
+import io.smallrye.reactive.messaging.annotations.Blocking;
+import io.smallrye.reactive.messaging.annotations.Merge;
+import io.vertx.core.json.JsonObject;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import tech.noetzold.model.InvoiceModel;
+import tech.noetzold.service.InvoiceService;
+
+@ApplicationScoped
+public class InvoiceConsumer {
+
+    @Inject
+    InvoiceService invoiceService;
+
+    private static final Logger logger = LoggerFactory.getLogger(CustomerConsumer.class);
+
+    @Incoming("invoicers")
+    @Merge
+    @Blocking
+    public InvoiceModel process(JsonObject incomingInvoiceModelInJson) {
+
+        InvoiceModel incomingInvoiceModel = incomingInvoiceModelInJson.mapTo(InvoiceModel.class);
+
+        invoiceService.saveInvoice(incomingInvoiceModel);
+        logger.info("Create Customer " + incomingInvoiceModel.getInvoiceId() + " for payment " + incomingInvoiceModel.getPayment().getPaymentId() + ".");
+
+        return incomingInvoiceModel;
+    }
+}

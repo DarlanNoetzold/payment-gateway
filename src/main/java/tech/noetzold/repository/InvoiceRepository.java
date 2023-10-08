36fbd376e6cbc458927fc47162a1bd39ddd37c2e
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-07 23:09:15 -0300

    Create InvoiceRepository.java

---
+++ src/main/java/tech/noetzold/repository/InvoiceRepository.java
@@ -0,0 +1,27 @@
+package tech.noetzold.repository;
+
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import tech.noetzold.model.CustomerModel;
+import tech.noetzold.model.InvoiceModel;
+import tech.noetzold.model.PaymentModel;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@ApplicationScoped
+public class InvoiceRepository implements PanacheRepository<InvoiceModel> {
+
+    public Optional<InvoiceModel> findByIdOptional(UUID id) {
+        return find("id", id).firstResultOptional();
+    }
+
+    public List<InvoiceModel> findByPaymentId(String paymentId) {
+        return find("payment", paymentId).list();
+    }
+
+    public void deleteById(UUID id) {
+        delete("id", id);
+    }
+}

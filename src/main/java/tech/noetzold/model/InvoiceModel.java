00751557f2d17e775ce5290423811525addf91fc
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-07 23:09:11 -0300

    Create InvoiceModel.java

---
+++ src/main/java/tech/noetzold/model/InvoiceModel.java
@@ -0,0 +1,54 @@
+package tech.noetzold.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.wildfly.common.annotation.NotNull;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class InvoiceModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private CustomerModel customer;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "paymentId")
+    private PaymentModel payment;
+
+    @NotNull
+    private String invoiceNumber;
+
+    @NotNull
+    private Date invoiceDate;
+
+    @NotNull
+    private double totalAmount;
+
+    private double discountAmount;
+
+    private String sellerName;
+
+    private String sellerAddress;
+
+    private String buyerName;
+
+    private String buyerAddress;
+
+    private List<String> itemsId;
+
+}
